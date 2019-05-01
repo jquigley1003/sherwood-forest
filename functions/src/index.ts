2d982eff32c66ba861067c25b4941a5b0e92798d
jquigley1003 <jeff.quigley@wavinghi.com>
2019-05-01 18:46:20 -0400

    create admin cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,6 +22,14 @@ export const createUser = functions.auth.user().onCreate((user) => {
   });
 });
 
+export const deleteUser = functions.firestore
+  .document('users/{userID}')
+  .onDelete((snap, context) => {
+    return admin.auth().deleteUser(snap.id)
+      .then(() => console.log('Deleted user with ID:' + snap.id))
+      .catch((error) => console.error('There was an error while deleting user:', error));
+  });
+
 export const sendContactMessage = functions.firestore
   .document('contactMessages/{messageId}')
   .onCreate(async (snapshot, context) => {
@@ -51,6 +59,42 @@ export const sendContactMessage = functions.firestore
       return null
     }
   });
+
+export const addAdmin = functions.https.onCall((data, context) => {
+  if (data.roles.admin !== true) {
+    return {
+      error: `Request not authorized. You must be an admin to grant request for ${data.email}.`
+    };
+  }
+  const userEmail = data.email;
+  return grantAdminRole(userEmail).then(() => {
+    const users = admin.firestore().collection('users');
+    return users.doc(data.uid).update({
+      roles: {
+        pendingMember: false,
+        approvedMember: true,
+        admin: true
+      }
+    })
+      .then(() => {
+        return {
+          result: `Request fulfilled and Firebase collection updated! ${userEmail} is now an admin.`
+        }
+      })
+  });
+});
+
+async function grantAdminRole(email: string): Promise<void> {
+  const user = await admin.auth().getUserByEmail(email);
+  if (user.customClaims && (user.customClaims as any).admin === true) {
+    return;
+  }
+  return admin.auth().setCustomUserClaims(user.uid, {
+    pendingMember: false,
+    approvedMember: true,
+    admin: true
+  });
+}
 
 function setEmailVerifiedTrue(uid: string) {
   return admin.auth().updateUser(uid, {
