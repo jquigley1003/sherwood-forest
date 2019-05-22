33d0458c4a559573e698f741728900f8ffa779e0
jquigley1003 <jeff.quigley@wavinghi.com>
2019-05-22 15:09:34 -0400

    update tree watch component

--- src/app/membership/tree-watch/tree-watch.page.ts
+++ src/app/membership/tree-watch/tree-watch.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-tree-watch',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TreeWatchPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+              private authService: AuthService) { }
 
-  ngOnInit() {
+  goHome() {
+    this.router.navigate(['/']);
+  }
+
+  logOut() {
+    this.authService.signOut();
   }
 
 }
