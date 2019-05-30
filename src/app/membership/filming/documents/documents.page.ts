deea8f1c9f515b4f61bbaf367e72388a426e6927
Jeff Quigley <jeffquigley@Jeffs-iMac.local>
2019-05-30 11:50:04 -0400

    update filming documents

--- src/app/membership/filming/documents/documents.page.ts
+++ src/app/membership/filming/documents/documents.page.ts
@@ -24,6 +24,14 @@ export class DocumentsPage implements OnInit {
     this.router.navigate(['/']);
   }
 
+  downloadFCC() {
+    window.open('https://firebasestorage.googleapis.com/v0/b/sherwood-forest-5b7f0.appspot.com/o/documents%2FFilmingCodeOfConduct.pdf?alt=media&token=8a22b996-803c-4cb5-b84a-e6bc05fd34cb');
+  }
+
+  downloadPG() {
+    window.open('https://firebasestorage.googleapis.com/v0/b/sherwood-forest-5b7f0.appspot.com/o/documents%2FProducerGuidelines.pdf?alt=media&token=03769db6-bba9-4298-93a2-9bb2fcd70d86');
+  }
+
   logOut() {
     this.authService.signOut();
   }
