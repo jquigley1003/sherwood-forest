06d4e83a94d80b78d62a569d156bf1408d7bf282
Jeff Quigley <jeffquigley@Jeffs-iMac.local>
2019-07-18 17:25:41 -0400

    adjust user modal form

--- src/app/shared/modals/pet-modal/pet-modal.module.ts
+++ src/app/shared/modals/pet-modal/pet-modal.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PetModalComponent } from './pet-modal.component';
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    PetModalComponent
+  ],
+  entryComponents: [
+    PetModalComponent
   ]
 })
 export class PetModalModule { }
