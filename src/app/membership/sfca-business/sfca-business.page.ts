1fadb7ff82c3ee07b6bf2ee9832a1c44aeba0037
jquigley1003 <jeff.quigley@wavinghi.com>
2022-12-05 19:10:38 -0500

    update business page with fall minutes

--- src/app/membership/sfca-business/sfca-business.page.ts
+++ src/app/membership/sfca-business/sfca-business.page.ts
@@ -32,6 +32,10 @@ export class SfcaBusinessPage implements OnInit {
     window.open('https://firebasestorage.googleapis.com/v0/b/sherwood-forest-5b7f0.appspot.com/o/documents%2FSFCAMinutes102219.pdf?alt=media&token=94e489a1-06c6-4506-ba0e-0eb5037d1b29');
   }
 
+  getMinutesFall22() {
+    window.open('https://firebasestorage.googleapis.com/v0/b/sherwood-forest-5b7f0.appspot.com/o/minutes%2F11152022%20SFCA%20Fall%20Meeting%20Minutes.doc.pdf?alt=media&token=43c3d0fc-1918-44bb-9c02-b2adbd31e901');
+  }
+
   getBusinessDocs() {
     window.open('https://firebasestorage.googleapis.com/v0/b/sherwood-forest-5b7f0.appspot.com/o/documents%2FSFCA%20BYLAWS%20%2003252020.pdf?alt=media&token=eacd4082-2336-4a03-8836-6f3e6e787e47');
   }
